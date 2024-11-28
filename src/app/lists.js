cfc664cc909f8fe112e308c95872c7e5ffb0167c
orgil-12 <theorgilbold@gmail.com>
2024-11-29 02:51:30 +0800

    fix the edit button and have to fix edit button

--- src/app/lists.js
+++ src/app/lists.js
@@ -18,7 +18,11 @@ const Modal = (props) => {
         <select
           name="status"
           id="select"
-          onChange={(e) => {props.setStatus(e.target.value);}} value={props.status}>
+          onChange={(e) => {
+            props.setStatus(e.target.value);
+          }}
+          value={props.status}
+        >
           <option>Select your status</option>
           <option value={"todo"}>To do</option>
           <option value={"inProgress"}>In progress</option>
@@ -33,19 +37,62 @@ const Modal = (props) => {
   );
 };
 
+const EditModal = (props) => {
+  return (
+    <div id="edit-task-pop-up" className="popup">
+      <p>Edit task</p>
+      <input
+        type="text"
+        placeholder="New task name..."
+        id="editedInput"
+        // defaultValue={props.title}
+        value={props.title}
+        onChange={(e) => props.setTitle(e.target.value)}
+      />
+      <label for="select">Choose your status :</label>
+      <select
+        name="status"
+        id="editedSelect"
+        value={props.status}
+        defaultValue={props.status}
+        onChange={(e) => props.setStatus(e.target.value)}
+      >
+        <option value="todo">To do</option>
+        <option value="inProgress">In progress</option>
+        <option value="done">Done</option>
+        <option value="blocked">Blocked</option>
+      </select>
+      <button onClick={props.handleEditSubmit}>Submit</button>
+    </div>
+  );
+};
+
 const Lists = () => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("");
+  const [id, setId] = useState(0);
 
-  const [tasks, setTodoTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
-  console.log(tasks);
+
+  const [isOpenEditModal, setIsOpenEditModal] = useState(false);
+
   const handleSubmit = () => {
-    setTodoTasks([...tasks, { title: title, status: status }]);
+    setTasks([...tasks, { id: id, title: title, status: status }]);
+    setId(id + 1);
     setIsOpen(false);
   };
+
+  const handleEditSubmit = () => {
+    setIsOpenEditModal(false);
+  };
+
   const openModal = () => {
     setIsOpen(!isOpen);
+  };
+
+  const openEditModal = () => {
+    setIsOpenEditModal(!isOpenEditModal);
   };
 
   return (
@@ -64,23 +111,11 @@ const Lists = () => {
           handleSubmit={handleSubmit}
         />
       )}
-      {/* <div id="edit-task-pop-up" className="popup">
-        <p>Edit task</p>
-        <input type="text" placeholder="New task name..." id="editedInput" />
-        <label for="select">Choose your status :</label>
-        <select name="status" id="editedSelect">
-          <option value="todo">To do</option>
-          <option value="inProgress">In progress</option>
-          <option value="done">Done</option>
-          <option value="blocked">Blocked</option>
-        </select>
-        <button >Submit</button>
-      </div> */}
       <main className="lists-section">
         <div className="to-do" id="todo">
           <div className="lists-title">
             <span className="dot"></span>
-            <p>To do</p>
+            <p className="title">To do</p>
             <span className="counter" id="todoCounter">
               0
             </span>
@@ -89,10 +124,25 @@ const Lists = () => {
             {tasks
               .filter((task) => task.status === "todo")
               .map((task) => (
-                <div className="task">
-                  <p>{task.title}</p>
-                  <img src="edit.png"></img>
-                  <img src="delete.png" className="delete-button"></img>
+                <div className="task" key={task.id}>
+                  <p className="task-name">{task.title}</p>
+                  <img src="edit.png" onClick={openEditModal}></img>
+                  {isOpenEditModal && (
+                    <EditModal
+                      title={title}
+                      setTitle={setTitle}
+                      status={status}
+                      setStatus={setStatus}
+                      handleEditSubmit={handleEditSubmit}
+                    />
+                  )}
+                  <img
+                    src="delete.png"
+                    className="delete-button"
+                    onClick={() => {
+                      setTasks(tasks.filter((taskk) => taskk.id !== task.id));
+                    }}
+                  ></img>
                 </div>
               ))}
           </div>
@@ -100,17 +150,26 @@ const Lists = () => {
         <div className="inProgress" id="inProgress">
           <div className="lists-title">
             <span className="dot"></span>
-            <p>In progress</p>
+            <p className="title">In progress</p>
             <span className="counter" id="inProgressCounter">
               0
             </span>
           </div>
           <div className="tasks" id="tasks-inProgress">
-            {tasks.filter((task) => task.status === "inProgress").map((task) => (
-                <div className="task">
-                  <p>{task.title}</p>
-                  <img src="edit.png"></img>
-                  <img src="delete.png" className="delete-button"></img>
+            {tasks
+              .filter((task) => task.status === "inProgress")
+              .map((task) => (
+                <div className="task" key={task.id}>
+                  <p className="task-name">{task.title}</p>
+                  <img src="edit.png" onClick={openEditModal}></img>
+                  {isOpenEditModal && <EditModal />}
+                  <img
+                    src="delete.png"
+                    className="delete-button"
+                    onClick={() => {
+                      setTasks(tasks.filter((taskk) => taskk.id !== task.id));
+                    }}
+                  ></img>
                 </div>
               ))}
           </div>
@@ -118,17 +177,26 @@ const Lists = () => {
         <div className="done" id="done">
           <div className="lists-title">
             <span className="dot"></span>
-            <p>Done</p>
+            <p className="title">Done</p>
             <span className="counter" id="doneCounter">
               0
             </span>
           </div>
           <div className="tasks" id="tasks-done">
-            {tasks.filter((task) => task.status === "done").map((task) => (
-                <div className="task">
-                  <p>{task.title}</p>
-                  <img src="edit.png"></img>
-                  <img src="delete.png" className="delete-button"></img>
+            {tasks
+              .filter((task) => task.status === "done")
+              .map((task) => (
+                <div className="task" key={task.id}>
+                  <p className="task-name">{task.title}</p>
+                  <img src="edit.png" onClick={openEditModal}></img>
+                  {isOpenEditModal && <EditModal />}
+                  <img
+                    src="delete.png"
+                    className="delete-button"
+                    onClick={() => {
+                      setTasks(tasks.filter((taskk) => taskk.id !== task.id));
+                    }}
+                  ></img>
                 </div>
               ))}
           </div>
@@ -136,17 +204,26 @@ const Lists = () => {
         <div className="blocked" id="blocked">
           <div className="lists-title">
             <span className="dot"></span>
-            <p>Blocked</p>
+            <p className="title">Blocked</p>
             <span className="counter" id="blockedCounter">
               0
             </span>
           </div>
           <div className="tasks" id="tasks-blocked">
-            {tasks.filter((task) => task.status === "blocked").map((task) => (
-                <div className="task">
-                  <p>{task.title}</p>
-                  <img src="edit.png"></img>
-                  <img src="delete.png" className="delete-button"></img>
+            {tasks
+              .filter((task) => task.status === "blocked")
+              .map((task) => (
+                <div className="task" key={task.id}>
+                  <p className="task-name">{task.title}</p>
+                  <img src="edit.png" onClick={openEditModal}></img>
+                  {isOpenEditModal && <EditModal />}
+                  <img
+                    src="delete.png"
+                    className="delete-button"
+                    onClick={() => {
+                      setTasks(tasks.filter((taskk) => taskk.id !== task.id));
+                    }}
+                  ></img>
                 </div>
               ))}
           </div>
